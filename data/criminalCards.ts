9cc83f201aa178bb083a76d5333241d23787c618
ra8200 <razi.alawamleh@gmail.com>
2024-08-07 12:55:21 -0400

    criminal law images added to cards

--- data/criminalCards.ts
+++ data/criminalCards.ts
@@ -21,109 +21,109 @@ interface CardData {
       title: "Battery",
       description:
         "Battery charges require a strong defense. Our attorneys at Kashlan Law have extensive experience defending clients against battery charges, ensuring that every aspect of your case is meticulously examined.",
-      image: "/criminal-law-3.jpg",
+      image: "/battery.png",
     },
     {
       title: "Aggravated Assault",
       description:
         "Aggravated assault charges can carry severe penalties. Kashlan Law provides aggressive legal defense for clients accused of aggravated assault, aiming to reduce charges and minimize consequences",
-      image: "/criminal-law-4.jpg",
+      image: "/aggravatedAssult.png",
     },
     {
       title: "Kidnapping",
       description:
         "Kidnapping charges are serious and require a skilled defense team. At Kashlan Law, we work diligently to challenge the prosecution's evidence and protect your rights.",
-      image: "/criminal-law-5.jpg",
+      image: "/kidnapping.png",
     },
     {
       title: "False Imprisonment",
       description:
         "False imprisonment charges can have significant legal repercussions. Our attorneys are committed to providing a strong defense, aiming to achieve the best possible outcome for your case.",
-      image: "/criminal-law-6.jpg",
+      image: "/falseImprisonment.png",
     },
     {
       title: "Cruelty to Children",
       description:
         "Accusations of cruelty to children are highly sensitive and complex. Kashlan Law offers compassionate and thorough defense strategies to protect your rights and reputation.",
-      image: "/criminal-law-7.jpg",
+      image: "/cruelty.png",
     },
     {
       title: "Sex Offenses",
       description:
         "Sex offense charges related to family violence are serious and can have long-lasting effects. Kashlan Law provides a rigorous defense to ensure your rights are upheld and your side of the story is heard.",
-      image: "/criminal-law-8.jpg",
+      image: "/sexOffense.png",
     },
     {
       title: "Burglary",
       description:
         "Charges of entering an auto with intent to commit theft are serious. Our attorneys are experienced in defending clients against these charges, working to protect your rights and achieve favorable outcomes.",
-      image: "/criminal-law-9.jpg",
+      image: "/burglary.png",
     },
     {
       title: "Entering Auto",
       description:
         "Charges of entering an auto with intent to commit theft are serious. Our attorneys are experienced in defending clients against these charges, working to protect your rights and achieve favorable outcomes.",
-      image: "/criminal-law-9.jpg",
+      image: "/enteringAuto.png",
     },
     {
       title: "Theft",
       description:
         "Theft charges can vary in severity. Kashlan Law offers comprehensive legal defense for all types of theft charges, from petty theft to grand larceny, ensuring your case is handled with the utmost care.",
-      image: "/criminal-law-9.jpg",
+      image: "/theft.png",
     },
     {
       title: "Forgery",
       description:
         "Forgery charges require a meticulous defense approach. At Kashlan Law, our attorneys are skilled in defending clients against forgery charges, challenging the evidence and protecting your rights.",
-      image: "/criminal-law-9.jpg",
+      image: "/forgery.png",
     },
     {
       title: "Identity Fraud",
       description:
         "Identity fraud charges can have serious implications. Our legal team at Kashlan Law is experienced in defending clients against identity fraud charges, striving to secure the best possible outcome.",
-      image: "/criminal-law-9.jpg",
+      image: "/identityFraud.png",
     },
     {
       title: "Financial Credit Card Fraud",
       description:
         "Financial credit card fraud charges are complex and require specialized legal knowledge. Kashlan Law provides expert defense strategies to protect your rights and fight for a favorable resolution.",
-      image: "/criminal-law-9.jpg",
+      image: "/financialFraud.png",
     },
     {
       title: "Criminal Trespass",
       description:
         "Charges of criminal damage to property require a strong defense. Kashlan Law offers experienced legal representation to defend against these charges, aiming to minimize penalties and protect your future.",
-      image: "/criminal-law-9.jpg",
+      image: "/trespass.png",
     },
     {
       title: "Obstructing Law Enforcement",
       description:
         "Obstructing law enforcement charges can complicate your legal situation. Our team at Kashlan Law provides robust defense strategies to challenge these charges and protect your rights.",
-      image: "/criminal-law-9.jpg",
+      image: "/obstructingLaw.png",
     },
     {
       title: "Terroristic Threats and Acts",
       description:
         "Charges of terroristic threats and acts are taken very seriously. Kashlan Law offers aggressive defense strategies to ensure your rights are defended and to work towards the best possible outcome.",
-      image: "/criminal-law-9.jpg",
+      image: "/terroristicThreats.png",
     },
     {
       title: "Possession of a Firearm by Convicted Felon",
       description:
         "Being charged with possession of a firearm by a convicted felon can result in severe penalties. Kashlan Law provides experienced legal defense to protect your rights and fight for a favorable resolution.",
-      image: "/criminal-law-9.jpg",
+      image: "/felonFirearm.png",
     },
     {
       title: "Trafficking & Intent to Distribute",
       description:
         "Charges of drug trafficking and intent to distribute require an aggressive defense. Our attorneys at Kashlan Law are experienced in handling these complex cases, working to secure the best possible outcome for our clients.",
-      image: "/criminal-law-9.jpg",
+      image: "/traffickingDistribute.png",
     },
     {
       title: "Possession of Illicit Substances",
       description:
         "Possession of illicit substances charges can have serious legal consequences. Kashlan Law offers expert defense strategies to challenge these charges and protect your future.",
-      image: "/criminal-law-9.jpg",
+      image: "/possession.png",
     },
   ];
   
