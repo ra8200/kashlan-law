5dbffba63c8d993a445a91d7be4f93ddd2dcff67
Razi Alawamleh <razi.alawamleh@gmail.com>
2024-08-09 10:25:48 -0400

    updated images and reanged them in public folder

--- data/criminalCards.ts
+++ data/criminalCards.ts
@@ -9,121 +9,121 @@ interface CardData {
       title: "RICO & Gang",
       description:
         "At Kashlan Law, we provide expert legal defense for those accused of RICO and gang-related offenses in the Northern District of Georgia. Our attorneys are experienced in handling complex cases involving organized crime and are dedicated to protecting your rights and achieving the best possible outcome.",
-      image: "/rico.png",
+      image: "/criminal/rico.png",
     },
     {
       title: "Murder",
       description:
         "Facing murder charges can be one of the most daunting experiences. Kashlan Law offers robust defense strategies for clients accused of murder in Georgia. We work tirelessly to ensure your rights are protected throughout the legal process.",
-      image: "/juvenile.png",
+      image: "/criminal/juvenile.png",
     },
     {
       title: "Battery",
       description:
         "Battery charges require a strong defense. Our attorneys at Kashlan Law have extensive experience defending clients against battery charges, ensuring that every aspect of your case is meticulously examined.",
-      image: "/battery.png",
+      image: "/criminal/battery.png",
     },
     {
       title: "Aggravated Assault",
       description:
         "Aggravated assault charges can carry severe penalties. Kashlan Law provides aggressive legal defense for clients accused of aggravated assault, aiming to reduce charges and minimize consequences",
-      image: "/aggravatedAssult.png",
+      image: "/criminal/aggravatedAssult.png",
     },
     {
       title: "Kidnapping",
       description:
         "Kidnapping charges are serious and require a skilled defense team. At Kashlan Law, we work diligently to challenge the prosecution's evidence and protect your rights.",
-      image: "/kidnapping.png",
+      image: "/criminal/kidnapping.png",
     },
     {
       title: "False Imprisonment",
       description:
         "False imprisonment charges can have significant legal repercussions. Our attorneys are committed to providing a strong defense, aiming to achieve the best possible outcome for your case.",
-      image: "/falseImprisonment.png",
+      image: "/criminal/falseImprisonment.png",
     },
     {
       title: "Cruelty to Children",
       description:
         "Accusations of cruelty to children are highly sensitive and complex. Kashlan Law offers compassionate and thorough defense strategies to protect your rights and reputation.",
-      image: "/cruelty.png",
+      image: "/criminal/cruelty.png",
     },
     {
       title: "Sex Offenses",
       description:
         "Sex offense charges related to family violence are serious and can have long-lasting effects. Kashlan Law provides a rigorous defense to ensure your rights are upheld and your side of the story is heard.",
-      image: "/sexOffense.png",
+      image: "/criminal/sexOffense.png",
     },
     {
       title: "Burglary",
       description:
         "Charges of entering an auto with intent to commit theft are serious. Our attorneys are experienced in defending clients against these charges, working to protect your rights and achieve favorable outcomes.",
-      image: "/burglary.png",
+      image: "/criminal/burglary.png",
     },
     {
       title: "Entering Auto",
       description:
         "Charges of entering an auto with intent to commit theft are serious. Our attorneys are experienced in defending clients against these charges, working to protect your rights and achieve favorable outcomes.",
-      image: "/enteringAuto.png",
+      image: "/criminal/enteringAuto.png",
     },
     {
       title: "Theft",
       description:
         "Theft charges can vary in severity. Kashlan Law offers comprehensive legal defense for all types of theft charges, from petty theft to grand larceny, ensuring your case is handled with the utmost care.",
-      image: "/theft.png",
+      image: "/criminal/theft.png",
     },
     {
       title: "Forgery",
       description:
         "Forgery charges require a meticulous defense approach. At Kashlan Law, our attorneys are skilled in defending clients against forgery charges, challenging the evidence and protecting your rights.",
-      image: "/forgery.png",
+      image: "/criminal/forgery.png",
     },
     {
       title: "Identity Fraud",
       description:
         "Identity fraud charges can have serious implications. Our legal team at Kashlan Law is experienced in defending clients against identity fraud charges, striving to secure the best possible outcome.",
-      image: "/identityFraud.png",
+      image: "/criminal/identityFraud.png",
     },
     {
       title: "Financial Credit Card Fraud",
       description:
         "Financial credit card fraud charges are complex and require specialized legal knowledge. Kashlan Law provides expert defense strategies to protect your rights and fight for a favorable resolution.",
-      image: "/financialFraud.png",
+      image: "/criminal/financialFraud.png",
     },
     {
       title: "Criminal Trespass",
       description:
         "Charges of criminal damage to property require a strong defense. Kashlan Law offers experienced legal representation to defend against these charges, aiming to minimize penalties and protect your future.",
-      image: "/trespass.png",
+      image: "/criminal/trespass.png",
     },
     {
       title: "Obstructing Law Enforcement",
       description:
         "Obstructing law enforcement charges can complicate your legal situation. Our team at Kashlan Law provides robust defense strategies to challenge these charges and protect your rights.",
-      image: "/obstructingLaw.png",
+      image: "/criminal/obstructingLaw.png",
     },
     {
       title: "Terroristic Threats and Acts",
       description:
         "Charges of terroristic threats and acts are taken very seriously. Kashlan Law offers aggressive defense strategies to ensure your rights are defended and to work towards the best possible outcome.",
-      image: "/terroristicThreats.png",
+      image: "/criminal/terroristicThreats.png",
     },
     {
       title: "Possession of a Firearm by Convicted Felon",
       description:
         "Being charged with possession of a firearm by a convicted felon can result in severe penalties. Kashlan Law provides experienced legal defense to protect your rights and fight for a favorable resolution.",
-      image: "/felonFirearm.png",
+      image: "/criminal/felonFirearm.png",
     },
     {
       title: "Trafficking & Intent to Distribute",
       description:
         "Charges of drug trafficking and intent to distribute require an aggressive defense. Our attorneys at Kashlan Law are experienced in handling these complex cases, working to secure the best possible outcome for our clients.",
-      image: "/traffickingDistribute.png",
+      image: "/criminal/traffickingDistribute.png",
     },
     {
       title: "Possession of Illicit Substances",
       description:
         "Possession of illicit substances charges can have serious legal consequences. Kashlan Law offers expert defense strategies to challenge these charges and protect your future.",
-      image: "/possession.png",
+      image: "/criminal/possession.png",
     },
   ];
   
--- data/personalCards.ts
+++ data/personalCards.ts
@@ -9,85 +9,85 @@ interface CardData {
       title: "Car Accidents",
       description:
         "At Kashlan Law, we understand that car accidents can be life-altering. Our experienced attorneys are dedicated to helping victims of car accidents in the Northern District of Georgia. We fight to secure compensation for medical expenses, lost wages, and pain and suffering. Trust us to handle the legal complexities while you focus on recovery.",
-      image: "/carAccident.png",
+      image: "/personal/car.png",
     },
     {
       title: "DUI Accidents",
       description:
         "DUI accidents often result in severe injuries and emotional trauma. At Kashlan Law, we aggressively pursue justice for victims of DUI accidents in Georgia. Our team works tirelessly to hold intoxicated drivers accountable and secure the compensation you deserve.",
-      image: "/dui.png",
+      image: "/personal/dui.png",
     },
     {
       title: "Truck Accidents",
       description:
         "Truck accidents can cause catastrophic injuries due to the sheer size and weight of commercial vehicles. Our attorneys have extensive experience handling truck accident cases in the Northern District of Georgia, ensuring that negligent parties are held responsible and victims receive just compensation.",
-      image: "/criminal-law-3.jpg",
+      image: "/personal/truck.png",
     },
     {
       title: "Rideshare Accidents",
       description:
         "Rideshare accidents involve unique legal challenges. At Kashlan Law, we specialize in navigating the complexities of rideshare accident claims, whether you're an injured passenger or another motorist. Our goal is to ensure you receive fair compensation for your injuries and losses.",
-      image: "/criminal-law-4.jpg",
+      image: "/personal/rideshare.png",
     },
     {
       title: "Motorcycle Accidents",
       description:
         "Motorcyclists are particularly vulnerable on the road. Our firm is committed to protecting the rights of motorcycle accident victims in Georgia. We work to secure compensation for medical bills, lost income, and other damages resulting from these often devastating accidents.",
-      image: "/criminal-law-5.jpg",
+      image: "/personal/motorcycle.png",
     },
     {
       title: "Bicycle Accidents",
       description:
         "Bicycle accidents can lead to serious injuries. At Kashlan Law, we advocate for the rights of injured cyclists in the Northern District of Georgia. Our attorneys are dedicated to holding negligent drivers accountable and obtaining the financial recovery you need to move forward.",
-      image: "/criminal-law-6.jpg",
+      image: "/personal/bicycle.png",
     },
     {
       title: "Pedestrian Accidents",
       description:
         "Pedestrian accidents frequently result in severe injuries. Our firm provides compassionate and aggressive representation for pedestrian accident victims. We work diligently to ensure you receive compensation for medical expenses, lost wages, and other related costs.",
-      image: "/criminal-law-7.jpg",
+      image: "/personal/pedestrian.png",
     },
     {
       title: "Dog Bites",
       description:
         "Dog bite injuries can be both physically and emotionally traumatic. At Kashlan Law, we help victims of dog bites in Georgia seek justice and compensation. Our attorneys are experienced in holding negligent pet owners accountable for the harm caused by their animals.",
-      image: "/criminal-law-8.jpg",
+      image: "/personal/dogbite.png",
     },
     {
       title: "Slip and Fall",
       description:
         "Slip and fall accidents can happen anywhere, from grocery stores to private properties. Our legal team at Kashlan Law is skilled in handling slip and fall cases, ensuring that property owners are held liable for unsafe conditions that cause injuries.",
-      image: "/criminal-law-9.jpg",
+      image: "/personal/slipfall.png",
     },
     {
       title: "Burn Injuries",
       description:
         "Burn injuries can have long-lasting effects. At Kashlan Law, we are dedicated to representing burn injury victims in the Northern District of Georgia. Our attorneys strive to secure compensation for medical treatments, rehabilitation, and other related expenses.",
-      image: "/criminal-law-9.jpg",
+      image: "/personal/burn.png",
     },
     {
       title: "Food and Beverage Injuries",
       description:
         "Food and beverage injuries, such as food poisoning or allergic reactions, can be serious. Kashlan Law provides expert legal assistance to victims of food and beverage injuries, helping them obtain compensation for medical costs and other damages.",
-      image: "/criminal-law-9.jpg",
+      image: "/personal/fnb.png",
     },
     {
       title: "Medical Malpractice",
       description:
         "Medical malpractice can have devastating consequences. Our attorneys at Kashlan Law are committed to holding healthcare providers accountable for negligence. We work tirelessly to ensure victims receive compensation for medical errors, misdiagnoses, and other forms of malpractice.",
-      image: "/criminal-law-9.jpg",
+      image: "/personal/medical.png",
     },
     {
       title: "Premises Liability",
       description:
         "Premises liability cases involve injuries sustained on someone else's property due to unsafe conditions. At Kashlan Law, we represent victims in premises liability cases, fighting for the compensation you deserve for your injuries.",
-      image: "/criminal-law-9.jpg",
+      image: "/personal/premises.png",
     },
     {
       title: "Wrongful Death",
       description:
         "Losing a loved one due to someone else's negligence is heartbreaking. Our compassionate attorneys at Kashlan Law provide dedicated legal support to families in wrongful death cases. We strive to obtain justice and financial relief for your loss.",
-      image: "/criminal-law-9.jpg",
+      image: "/personal/wrongfuldeath.png",
     },
   ];
   
