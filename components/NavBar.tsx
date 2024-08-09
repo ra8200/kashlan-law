13884a241ae02db8fa971e68602f25cd61135b44
Razi Alawamleh <razi.alawamleh@gmail.com>
2024-08-09 12:51:38 -0400

    again

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -7,7 +7,6 @@ import { Sheet, SheetTrigger, SheetContent } from "@/components/ui/sheet";
 import { Button } from "@/components/ui/button";
 import { usePathname } from "next/navigation";
 import { ChevronDownIcon, MenuIcon, PhoneIcon, ScaleIcon } from "@/components/icons";
-import Image from "next/image";
 
 export default function NavBar() {
   const pathname = usePathname();
@@ -37,7 +36,6 @@ export default function NavBar() {
             width={48}
             height={48} 
             className="w-12 h-10"
-            priority
           />
           <span className="text-lg font-bold">Kashlan Law</span>
         </Link>
